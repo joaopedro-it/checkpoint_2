bb2826b7d77f61256d67b1e5789e53616ed2c5d8
João Pedro Soler Antonio <Jpsoler2008@gmail.com>
2026-04-23 09:25:20 -0300

    Correção da linha 9

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,7 +1,10 @@
-#include "stdio.h"
+#include "stdio.h"\
 
 int main() {
     float nota1, nota2, media;
+    int opcao;
+
+    //Solicitação das notas
 
     printf("Digite a primeira nota: \n "); ///Adicionado ";" para correção do "printf"
     scanf("%f", &nota1);
@@ -9,12 +12,21 @@ int main() {
     printf("Digite a segunda nota: ");
     scanf("%f", nota2);
 
+    //Calculo da média
     media = nota1 + nota2 / 2;
 
     if (media = 6) {
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
+    }
+
+    //Menu de opções com switch case
+    {
+        printf("Escolha uma opção: \n");
+        printf("Calcula novamente: \n");
+        printf("Sair: \n");
+        scanf ("%d", &opcao);
     }
 
     return 0;
