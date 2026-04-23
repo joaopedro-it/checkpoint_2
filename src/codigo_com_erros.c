47f771ce98c6e2b5ebaa8aec274c1f5051ce61fc
João Pedro Soler Antonio <Jpsoler2008@gmail.com>
2026-04-23 09:38:09 -0300

    Switch case adicionado

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -27,7 +27,34 @@ int main() {
         printf("Calcula novamente: \n");
         printf("Sair: \n");
         scanf ("%d", &opcao);
-    }
+        switch (opcao){
+            case 1: 
+            printf("Digite novamente as notas: \n");
+            printf("Digite a primeira nota: \n");
+            scanf("%f, &nota1");
+            printf("Digite a segunda nota: \n");
+            scanf("%f, &nota2");
+
+        
+        media = (nota1 + nota2) / 2;
+        {
+
+                if (media >= 6) {
+                    printf("Aluno aprovado \n");
+                } else {
+                    printf("Aluno reprovado \n");
+                }
+                break;
+
+        case 2:
+            printf("Saindo...\n");
+            break;
+
+            default:
+            printf("Opção inválida! \n");
+        }
+    
+    };
 
     return 0;
     
